3ec7cf010541d061af4a4d8edf0e6c44d4082c95
Ottermanz <ottermanz1998@gmail.com>
2016-06-23 12:32:18 -0700

    Create helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,19 @@
+/************************
+* helloWorld.cpp
+* 
+* author: Otterman
+*
+* modifications:
+* 6/23/16 initial version
+************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+  cout << "Hello World! \n";
+  return 0;
+
+}
